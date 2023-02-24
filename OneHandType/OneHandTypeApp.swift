dabc91a4ddbb813176a2d2a053c9d165ed4adcd9
Gabi Garcia <gabigarcia@gabis-mbp.lan>
2023-02-24 15:46:14 -0600

    Initial Commit

---
+++ OneHandType/OneHandTypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneHandTypeApp.swift
+//  OneHandType
+//
+//  Created by Gabi Garcia on 2/24/23.
+//
+
+import SwiftUI
+
+@main
+struct OneHandTypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

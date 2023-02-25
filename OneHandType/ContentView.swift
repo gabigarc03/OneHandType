b63a012b258cea79237776a1ecfb1010e4b247ab
Gabi Garcia <gabigarcia@gabis-mbp.lan>
2023-02-25 15:32:43 -0600

    updated layout

--- OneHandType/ContentView.swift
+++ OneHandType/ContentView.swift
@@ -8,14 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tapped = false
+    @State private var entry = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        if !tapped {
+            VStack {
+                if !tapped {
+                    Button("Connect to Bluetooth!") {
+                        tapped.toggle()
+                    }.offset(y:350)
+                        .padding(.bottom, 275)
+                    
+                    List {
+                        Text("Bluetooth device 1")
+                        Text("Bluetooth device 2")
+                        Text("Bluetooth device 3")
+                        Text("Personal laptop")
+                    }.padding(.top, 200)
+                }
+                
+                if tapped {
+                    Text("Button tapped!").font(.largeTitle)
+                    Button("Disconnect") {
+                        tapped.toggle()
+                    }
+                    TextField("Enter text here", text: $entry, axis: .vertical)
+                }
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
